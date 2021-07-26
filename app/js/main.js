64d487083ef8f6a6e143b7fcb828dd0eb608ba91
unknown <pogrebnyak19999@gmail.com>
2021-07-26 21:51:02 +0300

    Создание оставшихся страниц, начало работы над адаптивом

--- app/js/main.js
+++ app/js/main.js
@@ -31,12 +31,63 @@ document.addEventListener("DOMContentLoaded", function(event) {
     } 
     // menu('', '', '');
 
+    // Открытие меню на ПК версии
+    function desktopMenu() {
+        
+        document.querySelector('.header__close').addEventListener('click', (item) => {
+            console.log(item.target)
+            document.querySelector('.menu').classList.remove('menu__open')
+            document.querySelector('.header__close').style.cssText = 'display: none'
+        })
+        document.querySelector('.header__open').addEventListener('click', (item) => {
+            console.log(item.target)
+            document.querySelector('.menu').classList.add('menu__open')
+            document.querySelector('.header__close').style.cssText = 'display: block'
+        })
+        document.querySelector('.menu__more').addEventListener('click', function () {
+            if (document.querySelector('.sub-menu').classList.contains('sub-menu__open')) {
+                document.querySelector('.menu__more svg').style.cssText = "transform: rotate(0deg)"
+                document.querySelector('.sub-menu').classList.remove('sub-menu__open')
+            } else {
+                document.querySelector('.menu__more svg').style.cssText = "transform: rotate(90deg)"
+                document.querySelector('.sub-menu').classList.add('sub-menu__open')
+            }
+        })
+    }
+    desktopMenu();
+
     $('.product-list__block').slick({
         dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 3,
-        arrows: true
+        arrows: true,
+        prevArrow: "<div class='prev'><img src='../img/svg/right-arrow.svg' alt='1'></div>",
+        nextArrow: "<div class='next'><img src='../img/svg/right-arrow.svg' alt='2'></div>",
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+
+          ]
     });
 
     $('.completed__boxImage').slick({
@@ -88,6 +139,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
         focusOnSelect: true,
         prevArrow: "<div class='prev'><img src='../img/svg/right-arrow.svg' alt='1'></div>",
         nextArrow: "<div class='next'><img src='../img/svg/right-arrow.svg' alt='2'></div>",
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+
+          ]
     });
 
 
@@ -95,7 +170,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         $(document).ready(function() {
             document.querySelectorAll('.product-slider__zoom').forEach(item => {
                 item.addEventListener('click', function() {
-                    document.querySelector('.modal__block img').src = this.parentElement.parentElement.querySelector('img').src
+                    document.querySelector('.modal__block img').src = this.parentElement.querySelector('img').src
                     document.querySelector('.modal').classList.add('modal__show')
                 })
                 document.querySelector('.modal').addEventListener('click', (item) => {
@@ -120,6 +195,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
         arrows: true,
         prevArrow: "<div class='prev'><img src='../img/svg/right-arrow.svg' alt='1'></div>",
         nextArrow: "<div class='next'><img src='../img/svg/right-arrow.svg' alt='2'></div>",
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+
+          ]
+    
     });
 
     $('.partner__slider').slick({
@@ -130,8 +230,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
         arrows: true,
         prevArrow: "<div class='prev'><img src='../img/svg/right-arrow.svg' alt='1'></div>",
         nextArrow: "<div class='next'><img src='../img/svg/right-arrow.svg' alt='2'></div>",
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+
+          ]
     });
 
+    function showOneNewsOnMobile() {
+        if (document.documentElement.clientWidth < 768) {
+            document.querySelectorAll('.news__item').forEach(item => {
+                for (let i = 1; i < document.querySelectorAll('.news__item').length; i++) {
+                    console.log(item)
+                    document.querySelectorAll('.news__item')[i].style.cssText = "display: none"
+                }
+            })
+
+            $('.equipment__block').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                fade: true,
+            });
+
+        }
+    }
+    showOneNewsOnMobile();
+
+    function productSliderAvailable() {
+        let postCount = document.querySelectorAll('.product-available__item').length;
+        if (postCount > 3) {
+            document.querySelector('.product-available__block').style.cssText = "overflow-x: scroll;"
+        }
+    }
+    productSliderAvailable();
 
     // Увеличение картинки сертификата на главной странице
     function openSertificate() {
